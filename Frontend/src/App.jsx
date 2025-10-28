304761e258e9d0c0f0185e14bbb9cac0806ef0de
ishikabhoyar <ishikabhoyar2005@gmail.com>
2025-10-28 14:14:44 +0530

    Remove Header component from ProtectedRoute in App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
                 path="/editor" 
                 element={
                   <ProtectedRoute>
-                    <Header />
+                    {/* <Header /> */}
                     <CodeChallenge />
                     <footer className="footer-bar fixed bottom-0 left-0 right-0 border-t border-slate-200/40 dark:border-gray-800/20 bg-black">
                       <div className="flex items-center justify-center h-7">
